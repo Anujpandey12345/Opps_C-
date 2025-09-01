0dc0b8b36d8fd6a98006dec785b5fc938823fac3
anujpandey12345 <anujpandey61687@gmail.com>
2025-09-02 03:12:27 +0530

    second

--- class1.cpp
+++ class1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
+// class firstclass{
+//     //Properties of class..
+//     int health;
+// };
+
+// int main(){
+//     //Object Creation...
+//     firstclass fc1;
+//     cout << "Size of fc1 is : "<< sizeof(fc1) << endl;
+
+//     return 0;
+// }
+
+
 class firstclass{
-    //Properties of class..
-    int health;
+    //Properties of class..Where we don't have any property means this is empty class...
 };
 
 int main(){
