c88008c75ae3bc6d90ae4d8dd979a4d040b03e63
anujpandey12345 <anujpandey61687@gmail.com>
2025-09-02 03:01:57 +0530

    first commit

---
+++ class1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class firstclass{
+    //Properties of class..
+    int health;
+};
+
+int main(){
+    //Object Creation...
+    firstclass fc1;
+    cout << "Size of fc1 is : "<< sizeof(fc1) << endl;
+
+    return 0;
+}
