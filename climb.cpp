356feaabb6b6612a3c4a456ba41beaf882700881
Anuj Kumar <anujpandey61687@gmail.com>
2025-09-14 14:11:19 +0530

    Climbing stair's

---
+++ climb.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int count(int i, int n, vector<int>&dp) {
+    if(i == n){
+        return 1;
+    }
+    if(i > n){
+        return 0;
+    }
+    if(dp[i] != -1){
+        return dp[i];
+    }
+    return dp[i] = count(i+1, n, dp)+count(i+2, n, dp);
+}
+int main() {
+    int n;
+    cout << "Enter the Number :: ";
+    cin >> n;
+    vector<int>dp(n+2, -1);
+    cout << "Stair's is :: " << count(0, n, dp) << endl;
+}
